3b4ce853bcd5de010850174d02192543f4ef301b
nikhilbhagat478 <bhagatnikhil012@gmail.com>
2023-12-31 21:10:34 +0530

    code until section 18

--- src/store/CartContext.jsx
+++ src/store/CartContext.jsx
@@ -4,6 +4,7 @@ const CartContext = createContext({
   items: [],
   addItem: (item) => {},
   removeItem: (id) => {},
+  clearCart: () => {},
 });
 
 function cartReducer(state, action) {
@@ -47,7 +48,9 @@ function cartReducer(state, action) {
 
     return { ...state, items: updatedItems };
   }
-
+  if (action.type === "CLEAR_CART") {
+    return { ...state, items: [] };
+  }
   return state;
 }
 
@@ -64,10 +67,15 @@ export function CartContextProvider({ children }) {
     dispatchCartAction({ type: "REMOVE_ITEM", id });
   }
 
+  function clearCart() {
+    dispatchCartAction({ type: "CLEAR_CART" });
+  }
+
   const cartContext = {
     items: cart.items,
     addItem,
     removeItem,
+    clearCart,
   };
 
   return (
